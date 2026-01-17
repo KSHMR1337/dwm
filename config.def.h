e48855d0406316a03a490ed9d94cc22d7dfb15c7
KSHMR1337 <marko.zare1234@hotmail.com>
2026-01-17 01:07:04 +0100

    Added movestack patch and cleaned config formatting.

--- config.def.h
+++ config.def.h
@@ -1,21 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 
+#include "movestack.c"
+
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int gappx = 10;   /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const int swallowfloating =
-    0;                        /* 1 means swallow floating windows by default */
+static const int swallowfloating = 0;                        /* 1 means swallow floating windows by default */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
-static const int user_bh =
-    6; /* 2 is the default spacing around the bar's font */
+static const int user_bh = 6; /* 2 is the default spacing around the bar's font */
 static const int vertpad = 10; /* vertical padding of bar */
 static const int sidepad = 15; /* horizontal padding of bar */
-static const char *fonts[] = {
-    "Hack Nerd Font Mono:pixelsize=15:antialias=true:autohint=true"};
-static const char dmenufont[] =
-    "Hack Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
+static const char *fonts[] = { "Hack Nerd Font Mono:pixelsize=15:antialias=true:autohint=true"};
+static const char dmenufont[] =  "Hack Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
 
 /* Red flavour colors */
 
@@ -108,106 +106,91 @@ static const Layout layouts[] = {
 #define STATUSBAR "dwmblocks"
 
 /* commands */
-static char dmenumon[2] =
-    "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"dmenu_run", "-c", "-g",     "3",   "-l",
-                                 "10",        "-m", dmenumon, "-fn", dmenufont,
-                                 "-bw",       "3",  NULL};
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = {"dmenu_run", "-c", "-g",     "3",   "-l", "10",        "-m", dmenumon, "-fn", dmenufont, "-bw", "3",  NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *tabtermcmd[] = {"tabbed", "-r", "2", "st", "-w", "''", NULL};
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = {"st", "-t",     scratchpadname,
-                                      "-g", "120x34", NULL};
+static const char *scratchpadcmd[] = {"st", "-t",     scratchpadname, "-g", "120x34", NULL};
 
 static const Key keys[] = {
-    /* modifier                     key        function        argument */
-    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
-    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
-    {ALTMOD | ShiftMask, XK_Return, spawn, {.v = tabtermcmd}},
-    {MODKEY, XK_grave, togglescratch, {.v = scratchpadcmd}},
-    {MODKEY, XK_b, togglebar, {0}},
-    {MODKEY, XK_j, focusstackvis, {.i = +1}},
-    {MODKEY, XK_k, focusstackvis, {.i = -1}},
-    {MODKEY | ShiftMask, XK_j, focusstackhid, {.i = +1}},
-    {MODKEY | ShiftMask, XK_k, focusstackhid, {.i = -1}},
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_d, incnmaster, {.i = -1}},
-    {MODKEY, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_Return, zoom, {0}},
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY | ShiftMask, XK_c, killclient, {0}},
-    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
-    {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
-    {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
-    {MODKEY, XK_u, setlayout, {.v = &layouts[3]}},
-    {MODKEY, XK_o, setlayout, {.v = &layouts[4]}},
-    {MODKEY, XK_space, setlayout, {0}},
-    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-    {ALTMOD | ShiftMask, XK_Tab, focusmon, {.i = -1}},
-    {ALTMOD, XK_Tab, focusmon, {.i = +1}},
-    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
-    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    {MODKEY, XK_Right, viewnext, {0}},
-    {MODKEY, XK_Left, viewprev, {0}},
-    {MODKEY | ShiftMask, XK_Right, tagtonext, {0}},
-    {MODKEY | ShiftMask, XK_Left, tagtoprev, {0}},
-    {MODKEY, XK_a, show, {0}},
-    {MODKEY | ShiftMask, XK_a, showall, {0}},
-    {ALTMOD, XK_h, hide, {0}},
-    {MODKEY, XK_minus, setgaps, {.i = -1}},
-    {MODKEY, XK_equal, setgaps, {.i = +1}},
-    {MODKEY | ShiftMask, XK_equal, setgaps, {.i = 0}},
-    {MODKEY | ShiftMask, XK_s, xrdb, {.v = NULL}},
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+    /* modifier                          key                       function        argument */
+    { MODKEY,                            XK_p,                     spawn,          {.v = dmenucmd} },
+    { MODKEY|ShiftMask,                  XK_Return,                spawn,          {.v = termcmd} },
+    { ALTMOD|ShiftMask,                  XK_Return,                spawn,          {.v = tabtermcmd} },
+    { MODKEY,                            XK_grave,                 togglescratch,  {.v = scratchpadcmd} },
+    { MODKEY,                            XK_b,                     togglebar,      {0} },
+    { MODKEY,                            XK_j,                     focusstackvis,  {.i = +1} },
+    { MODKEY,                            XK_k,                     focusstackvis,  {.i = -1} },
+    { MODKEY|ShiftMask,                  XK_j,                     focusstackhid,  {.i = +1} },
+    { MODKEY|ShiftMask,                  XK_k,                     focusstackhid,  {.i = -1} },
+    { MODKEY,                            XK_i,                     incnmaster,     {.i = +1} },
+    { MODKEY,                            XK_d,                     incnmaster,     {.i = -1} },
+    { MODKEY,                            XK_h,                     setmfact,       {.f = -0.05} },
+    { MODKEY,                            XK_l,                     setmfact,       {.f = +0.05} },
+    { MODKEY|ShiftMask|ControlMask,      XK_j,                     movestack,      {.i = +1} },
+    { MODKEY|ShiftMask|ControlMask,      XK_k,                     movestack,      {.i = -1} },
+    { MODKEY,                            XK_Return,                zoom,           {0} },
+    { MODKEY,                            XK_Tab,                   view,           {0} },
+    { MODKEY|ShiftMask,                  XK_c,                     killclient,     {0} },
+    { MODKEY,                            XK_t,                     setlayout,      {.v = &layouts[0]} },
+    { MODKEY,                            XK_f,                     setlayout,      {.v = &layouts[1]} },
+    { MODKEY,                            XK_m,                     setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                            XK_u,                     setlayout,      {.v = &layouts[3]} },
+    { MODKEY,                            XK_o,                     setlayout,      {.v = &layouts[4]} },
+    { MODKEY,                            XK_space,                 setlayout,      {0} },
+    { MODKEY|ShiftMask,                  XK_space,                 togglefloating, {0} },
+    { MODKEY,                            XK_0,                     view,           {.ui = ~0} },
+    { MODKEY|ShiftMask,                  XK_0,                     tag,            {.ui = ~0} },
+    { ALTMOD|ShiftMask,                  XK_Tab,                   focusmon,       {.i = -1} },
+    { ALTMOD,                            XK_Tab,                   focusmon,       {.i = +1} },
+    { MODKEY|ShiftMask,                  XK_comma,                 tagmon,         {.i = -1} },
+    { MODKEY|ShiftMask,                  XK_period,                tagmon,         {.i = +1} },
+    { MODKEY,                            XK_Right,                 viewnext,       {0} },
+    { MODKEY,                            XK_Left,                  viewprev,       {0} },
+    { MODKEY|ShiftMask,                  XK_Right,                 tagtonext,      {0} },
+    { MODKEY|ShiftMask,                  XK_Left,                  tagtoprev,      {0} },
+    { MODKEY,                            XK_a,                     show,           {0} },
+    { MODKEY|ShiftMask,                  XK_a,                     showall,        {0} },
+    { ALTMOD,                            XK_h,                     hide,           {0} },
+    { MODKEY,                            XK_minus,                 setgaps,        {.i = -1} },
+    { MODKEY,                            XK_equal,                 setgaps,        {.i = +1} },
+    { MODKEY|ShiftMask,                  XK_equal,                 setgaps,        {.i = 0} },
+    { MODKEY|ShiftMask,                  XK_s,                     xrdb,           {.v = NULL} },
 
-    {0, XF86XK_AudioLowerVolume, spawn,
-     SHCMD("/usr/bin/pamixer -d 5;kill -44 $(pidof dwmblocks)")},
-    {0, XF86XK_AudioMute, spawn,
-     SHCMD("/usr/bin/pamixer -t;kill -44 $(pidof dwmblocks)")},
-    {0, XF86XK_AudioRaiseVolume, spawn,
-     SHCMD("/usr/bin/pamixer -i 5;kill -44 $(pidof dwmblocks)")},
-    {0, XF86XK_MonBrightnessUp, spawn,
-     SHCMD("/usr/bin/xbacklight -inc 5;kill -40 $(pidof dwmblocks)")},
-    {0, XF86XK_MonBrightnessDown, spawn,
-     SHCMD("/usr/bin/xbacklight -dec 5;kill -40 $(pidof dwmblocks)")},
-    {0, XF86XK_AudioPrev, spawn, SHCMD("/usr/bin/mpc prev")},
-    {0, XF86XK_AudioPlay, spawn, SHCMD("/usr/bin/mpc toggle")},
-    {0, XF86XK_AudioNext, spawn, SHCMD("/usr/bin/mpc next")},
-    {0, XF86XK_Search, spawn, SHCMD("$BROWSER")},
-    {0, XK_Print, spawn,
-     SHCMD("/usr/bin/maim -u | xclip -selection clipboard -t image/png; xclip "
-           "-out -selection clipboard > ~/Pictures/Screenshots/$(date "
-           "'+%F-%H-%M-%S').png")},
-    {ShiftMask, XK_Print, spawn,
-     SHCMD("/usr/bin/maim -su | xclip -selection clipboard -t image/png; xclip "
-           "-out -selection clipboard > ~/Pictures/Screenshots/$(date "
-           "'+%F-%H-%M-%S').png")},
+    TAGKEYS(                             XK_1,                                       0)
+    TAGKEYS(                             XK_2,                                       1)
+    TAGKEYS(                             XK_3,                                       2)
+    TAGKEYS(                             XK_4,                                       3)
+    TAGKEYS(                             XK_5,                                       4)
+    TAGKEYS(                             XK_6,                                       5)
+    TAGKEYS(                             XK_7,                                       6)
+    TAGKEYS(                             XK_8,                                       7)
+    TAGKEYS(                             XK_9,                                       8)
 
-    {ALTMOD,
-     XK_v,
-     spawn,
-     {.v = (const char *[]){"dmenu_cliphist", "sel", NULL}}},
-    {MODKEY,
-     XK_c,
-     spawn,
-     {.v = (const char *[]){"dmenu_cliphist", "add", NULL}}},
-    {MODKEY | ShiftMask,
-     XK_Escape,
-     spawn,
-     {.v = (const char *[]){"dmenu_sys", NULL}}},
-    {ALTMOD, XK_Escape, spawn, SHCMD("~/.local/bin/change_keyboard_layout.sh")},
-    {MODKEY, XK_s, spawn, SHCMD("~/.local/bin/change_theme.sh")},
+    { MODKEY|ShiftMask,                  XK_q,                     quit,           {0} },
+    { 0,                                 XF86XK_AudioLowerVolume,  spawn,          SHCMD("/usr/bin/pamixer -d 5;kill -44 $(pidof dwmblocks)") },
+    { 0,                                 XF86XK_AudioMute,         spawn,          SHCMD("/usr/bin/pamixer -t;kill -44 $(pidof dwmblocks)") },
+    { 0,                                 XF86XK_AudioRaiseVolume,  spawn,          SHCMD("/usr/bin/pamixer -i 5;kill -44 $(pidof dwmblocks)") },
+    { 0,                                 XF86XK_MonBrightnessUp,   spawn,          SHCMD("/usr/bin/xbacklight -inc 5;kill -40 $(pidof dwmblocks)") },
+    { 0,                                 XF86XK_MonBrightnessDown, spawn,          SHCMD("/usr/bin/xbacklight -dec 5;kill -40 $(pidof dwmblocks)") },
+    { 0,                                 XF86XK_AudioPrev,         spawn,          SHCMD("/usr/bin/mpc prev") },
+    { 0,                                 XF86XK_AudioPlay,         spawn,          SHCMD("/usr/bin/mpc toggle") },
+    { 0,                                 XF86XK_AudioNext,         spawn,          SHCMD("/usr/bin/mpc next") },
+    { 0,                                 XF86XK_Search,            spawn,          SHCMD("$BROWSER") },
+    { 0,                                 XK_Print,                 spawn,          SHCMD("/usr/bin/maim -u | xclip -selection clipboard -t image/png; xclip -out -selection clipboard > ~/Pictures/Screenshots/$(date '+%F-%H-%M-%S').png") },
+    { ShiftMask,                         XK_Print,                 spawn,          SHCMD("/usr/bin/maim -su | xclip -selection clipboard -t image/png; xclip -out -selection clipboard > ~/Pictures/Screenshots/$(date '+%F-%H-%M-%S').png") },
 
-    {MODKEY | ALTMOD | ShiftMask, XK_s, spawn, SHCMD("pavucontrol")},
-    {MODKEY | ALTMOD | ShiftMask, XK_b, spawn, SHCMD("blueman-manager")},
-    {MODKEY | ALTMOD | ShiftMask, XK_d, spawn, SHCMD("discord")},
-    {MODKEY | ALTMOD | ShiftMask, XK_o, spawn, SHCMD("obsidian")},
-    {MODKEY | ALTMOD | ShiftMask, XK_v, spawn, SHCMD("stremio")},
+    { ALTMOD,                            XK_v,                     spawn,          {.v = (const char *[]){"dmenu_cliphist", "sel", NULL}} },
+    { MODKEY,                            XK_c,                     spawn,          {.v = (const char *[]){"dmenu_cliphist", "add", NULL}} },
+    { MODKEY|ShiftMask,                  XK_Escape,                spawn,          {.v = (const char *[]){"dmenu_sys", NULL}} },
+    { ALTMOD,                            XK_Escape,                spawn,          SHCMD("~/.local/bin/change_keyboard_layout.sh") },
+    { MODKEY,                            XK_s,                     spawn,          SHCMD("~/.local/bin/change_theme.sh") },
+    { MODKEY|ALTMOD|ShiftMask,           XK_s,                     spawn,          SHCMD("pavucontrol") },
+    { MODKEY|ALTMOD|ShiftMask,           XK_b,                     spawn,          SHCMD("blueman-manager") },
+    { MODKEY|ALTMOD|ShiftMask,           XK_d,                     spawn,          SHCMD("discord") },
+    { MODKEY|ALTMOD|ShiftMask,           XK_o,                     spawn,          SHCMD("obsidian") },
+    { MODKEY|ALTMOD|ShiftMask,           XK_v,                     spawn,          SHCMD("stremio") },
 };
 
 /* button definitions */
